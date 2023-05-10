1298b6404d1540d5bc18c72b92b16c5e14dd59bd
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-10 16:45:32 +0530

    use of predefined insert() method

---
+++ JAVA_PROGRAMMING/CLASS_CODES/StringBuffer/StringBuffer_Methods/Predefined_Methods/insert.java
@@ -0,0 +1,19 @@
+
+// insert() method :
+
+class preMethod{
+    public static void main(String[] args) {
+        
+        StringBuffer str1 = new StringBuffer("RushiNashik");
+
+        System.out.println(str1);
+
+        // use of insert method
+
+        str1.insert(5,"Birari");
+
+        System.out.println(str1);
+
+    }
+
+}
