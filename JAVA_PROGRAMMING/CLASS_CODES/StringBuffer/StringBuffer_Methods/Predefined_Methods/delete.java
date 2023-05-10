21321e2030ad76d4b5c47582c7485252bf58ae5f
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-10 16:45:57 +0530

    use of predefined delete() method

---
+++ JAVA_PROGRAMMING/CLASS_CODES/StringBuffer/StringBuffer_Methods/Predefined_Methods/delete.java
@@ -0,0 +1,19 @@
+
+// delete() method :
+
+class preMethod{
+    public static void main(String[] args) {
+        
+        StringBuffer str1 = new StringBuffer("RushiBirari");
+
+        System.out.println(str1);
+
+        // use of delete method
+
+       str1.delete(5, 8);
+
+        System.out.println(str1);
+
+    }
+
+}
