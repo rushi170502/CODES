4ac9dcdbc7f564503d4402ef152fc0c602c8df8c
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-15 14:52:28 +0530

    value initialisation

---
+++ C_PROGRAMMING/C_PROGRAMS/Pointers/Wild_Null_pointer/program4.c
@@ -0,0 +1,18 @@
+
+
+
+// 
+#include<stdio.h>
+void main(){
+
+	int x=10;
+
+	int y=20;
+
+	printf("%d\t%d\n",x,y);// 10  20 
+
+	x=y;  // assigning the value of y to x..
+
+	printf("%d\t%d\n",x,y);// 20 20
+
+}
