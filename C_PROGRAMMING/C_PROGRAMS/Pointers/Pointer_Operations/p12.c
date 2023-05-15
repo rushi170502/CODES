cc6305cecf49bd459eac836b24525c711ea6961d
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-15 14:42:18 +0530

    Increment operation on pointers...

---
+++ C_PROGRAMMING/C_PROGRAMS/Pointers/Pointer_Operations/p12.c
@@ -0,0 +1,21 @@
+//
+//
+//Increment operation on pointers,,,'''
+//
+#include<stdio.h>
+void main(){
+
+	int arr[]={10,20,30,40,50};
+
+	int *ptr=&(arr[0]);
+
+	ptr++;
+
+	printf("%d\n",*ptr);
+
+
+	++ptr;
+
+	printf("%d\n",*ptr);
+
+}
