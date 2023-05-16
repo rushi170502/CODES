5db3e69c7c75d910755573fe7dd60e40ba0cb1f1
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-16 13:48:50 +0530

    WAP to implement stack  use PUSH, POP, PEEK functions

---
+++ C_PROGRAMMING/Data Structure & Algorithm/STACK/Stack_using_Array/program5.c
@@ -0,0 +1,131 @@
+
+
+// WAP to implement the stack using array ..
+//
+  //1. push 
+  //2. pop
+  //3. peek
+  
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int top = -1;
+
+int size = 0;
+int flag = 0;
+
+int push(int stack[]){
+
+	if(top == size -1){
+		printf("\nStack overflow....u can't push data\n");
+		return -1;
+	}
+	else{
+		int data;
+		printf("\nEnter the data  :  ");
+		scanf("%d",&data);
+
+		top++;
+		stack[top] = data;
+	
+		printf("\n%d added...\n",data);
+		return 0;
+	}
+		
+
+}
+
+int pop(int stack[]){
+
+
+	if(top == -1){
+		flag = 0;
+
+		printf("\nStack underflow...\n");
+		return -1;
+	}else{
+
+		int val = stack[top];
+
+		flag = 1;
+
+		top--;
+
+		return val;
+	}
+
+}
+
+int peek(int stack[]){
+
+	if(top == -1){
+
+		flag = 0;
+		return -1;
+	}
+	else{
+		flag = 1;
+		return stack[top];
+	}
+
+}
+
+void main(){
+
+	int ch;
+	printf("\nEnter the size of stack  :  ");
+	scanf("%d",&size);
+
+	int stack[size];
+
+	while(1){
+
+		printf("\t!!******** M E N U ***********!!\n");
+		printf("\t press 1 : push data\n");
+		printf("\t press 2 : pop data\n");
+		printf("\t press 3 : peek \n");
+		printf("\t press 4 : exit\n\n");
+
+		printf("\n Enter the choice  :  ");
+		scanf("%d",&ch);
+
+		switch(ch){
+
+			case 1:
+				push(stack);
+				break;
+
+			case 2:{
+				int data = pop(stack);
+
+				if(flag == 0)
+					break;
+				else{
+					printf("\n%d is popped..\n\n",data);
+				       }
+			       }
+				break;
+
+			case 3:{
+				int val	=peek(stack);
+				if(flag == 0)
+					printf("\n Stack is an underflow..\n");
+				else
+					printf(" %d  \n",val);
+			       }
+				break;
+
+			case 4:
+				exit(0);
+				break;
+
+			default:
+				printf("\nWrong choice....\n");
+
+		}
+
+		}
+	}
+				
+
