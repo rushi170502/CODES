a982336199c150f43d7de78ba54171745e775593
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-19 23:09:22 +0530

    WAP to implement the queue using array...

---
+++ C_PROGRAMMING/Data Structure & Algorithm/QUEUE/Normal_Queue/program1.c
@@ -0,0 +1,31 @@
+
+// WAP to implement the queue using array...
+//
+
+#include<stdio.h>
+
+
+int size =0;
+
+
+
+void main(){
+
+	int ch;
+
+	int size;
+
+	printf("\nEnter the size of queue :  ");
+	scanf("%d",&size);
+
+	int arr[size];
+
+	while(1){
+
+		printf("\n\t!!*************M E N U***************\n");
+		printf("\nPress 1: enqueue data\n");
+		printf("\nPress 2: dequeue data\n");
+		printf("\nPress 3: enqueue data\n");
+		printf("\nPress 4: enqueue data\n");
+		printf("\nPress 5: enqueue data\n");
+		printf("\nPress 1: enqueue data\n");
