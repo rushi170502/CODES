a0f8362a84256114da1a577550a4f41fe773825a
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-19 22:54:53 +0530

    WAP to print the index of element of which is prime in array

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/ARRAY/Array_03/program4.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+class ArrayDemo{
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the size of array :");
+        int size = Integer.parseInt(br.readLine());
+        int arr[] = new int[size];
+        System.out.println("Enter the elements of array : ");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        for(int i = 0 ; i < arr.length; i++){
+            int count = 0;
+            for(int j = 1; j <= arr[i]; j++) {
+                if (arr[i] % j == 0)
+                    count++;
+            }
+            if(count <= 2) {
+                System.out.print("Prime number " + arr[i] + " found at " + i + " index");
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+}
