f95e77256255db3b57bb9a296b1bfabaf0cd6b02
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-19 22:55:49 +0530

    WAP to find the palindrome number in array and return its index

---
+++ JAVA_PROGRAMMING/ASSIGNMENTS/ARRAY/Array_03/program6.java
@@ -0,0 +1,28 @@
+import java.io.*;
+class ArrayDemo{
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the size of array :");
+        int size = Integer.parseInt(br.readLine());
+        int arr[] = new int[size];
+        System.out.println("Enter the elements of array : ");
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        for(int i = 0 ; i < arr.length; i++) {
+
+            int rev = 0;
+            int num = arr[i];
+
+            while(num != 0){
+                int rem = num % 10;
+                rev = rev * 10 + rem;
+                num /= 10;
+            }
+            if(rev == arr[i])
+                System.out.println("Palindrome number "+arr[i]+" found at "+i+ " index");
+        }
+        System.out.println();
+    }
+}
