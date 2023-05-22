fbce234e5730f3108b39d5d0a11721698b32a8b7
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-23 00:09:26 +0530

    Simple program 2 of class and object creation

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Classes_&_Objects/Simple/program2.java
@@ -0,0 +1,15 @@
+
+class Demo{
+
+    String str = "Shashi";
+    
+    void fun(){
+        String str1 = "Hello";
+        String str2 = new String("World");
+    }
+
+    public static void main(String[] args) {
+        Demo obj = new Demo();
+        obj.fun();
+    }
+}
