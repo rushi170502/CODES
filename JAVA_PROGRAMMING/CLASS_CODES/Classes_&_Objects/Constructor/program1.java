6548caa49c2fd02e229137ed63e44b62abb015fa
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-23 00:10:51 +0530

    Simple implementation of constructor local/global variables

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Classes_&_Objects/Constructor/program1.java
@@ -0,0 +1,13 @@
+
+class Demo{
+
+    int y = 20; // instance variable
+
+    void fun(){
+        int z = 30; // local variable for fun() function
+    }
+    public static void main(String[] args) {
+        int x = 10;// local variable
+        System.out.println(x);
+    }
+}
