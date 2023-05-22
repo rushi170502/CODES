c14c4897e167c9e1a7788d0f7ecb69e0c347f9e7
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-05-23 00:12:03 +0530

    Simple implementation of constructor local/global static & non-static variables and static methods

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Classes_&_Objects/Constructor/program3.java
@@ -0,0 +1,20 @@
+
+
+class Demo{
+    
+    int x = 10;
+    int num = 800;
+    static int jerNo = 18;
+
+    void fun(){
+        int z = 20;
+    }
+
+    static void add(){
+        int num = 200;
+
+
+    }
+
+    static float y = 90;
+}
