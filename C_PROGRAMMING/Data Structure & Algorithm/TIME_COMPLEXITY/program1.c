05f652e0064ff7de3c0af7ae446f23f2b049f5c2
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-06-01 23:02:18 +0530

    WAP find factor and measuring the iteration of code

---
+++ C_PROGRAMMING/Data Structure & Algorithm/TIME_COMPLEXITY/program1.c
@@ -0,0 +1,30 @@
+
+// WAP find factor
+// measuring the iteration of code
+
+
+#include<stdio.h>
+
+void fun(int n){
+
+	int i= 1;// itr = 1
+	while(i <= n){ // itr = N
+
+		if(n % i == 0)//itr = 2N (two task  % & ==)
+			printf("%d  ",i);// itr = N
+
+		i++;// itr = 2N(two task i = i+1 ( +, =))
+
+	}
+}
+		// total iteration : 
+		// = 1 + N + 2N + N + 2N
+		// = 1+ 6N
+
+void main(){
+
+	int n = 35;
+
+	fun(n);
+	printf("\n\n");
+}
