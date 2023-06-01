b94645d492925595b4383a7c6fca914c20d99dbd
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-06-01 23:05:09 +0530

    Find the no. of iterations and Time complexity

---
+++ C_PROGRAMMING/Data Structure & Algorithm/TIME_COMPLEXITY/program6.c
@@ -0,0 +1,19 @@
+//
+// Find the no. of iterations and Time complexity
+
+#include<stdio.h>
+
+void main(){
+
+	int N = 10;
+
+	for(int i = 1; i * i <= N; i++ ){
+
+		//-----;
+
+	}
+}
+
+// total iteration :  =  sqrt(N)
+//
+// Time complexity : 	O(sqrt(N))
