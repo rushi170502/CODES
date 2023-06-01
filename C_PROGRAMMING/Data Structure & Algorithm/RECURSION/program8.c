b5531819131aa2841dd55eb8294c57d71a35c2ed
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-06-01 22:26:36 +0530

    What is the return value of function if fun(345,10)  [Code of sum of digits]

---
+++ C_PROGRAMMING/Data Structure & Algorithm/RECURSION/program8.c
@@ -0,0 +1,21 @@
+
+// Q. What is the return value of function if fun(345,10)  [Code of sum of digits]
+
+
+#include<stdio.h>
+
+int foo(int n, int r){
+
+	if(n > 0)// recursive function call
+		return (n%r+ foo(n/r,r));
+	else
+		return 0;
+
+}
+void main(){
+
+	int x = 3459, y = 10;
+
+	int ret = foo(x,y);
+	printf("sum  =  %d \n\n",ret);
+}
