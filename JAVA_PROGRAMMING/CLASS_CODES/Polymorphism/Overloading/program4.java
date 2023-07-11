5d168002c9143d8940d4a51ce8f0b9b98992d93f
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-07-11 11:06:43 +0530

    Concept in overloading method name same but parameter list is different.

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Polymorphism/Overloading/program4.java
@@ -0,0 +1,17 @@
+
+
+class Solution{
+
+    int fun(int x){
+
+    }
+
+    int fun(float y){
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
+
+// No error : Because both methods have different arguments list because of method signatures...
