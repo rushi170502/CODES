204578d0cc712e39fbd86269e2d9a4868d94f4ef
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-07-11 11:03:20 +0530

    concept of duplicate method signatures.

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Polymorphism/Overloading/program2.java
@@ -0,0 +1,15 @@
+
+class Solution{
+
+    int add(int a, int b){
+        System.out.println("method add 1");
+    }
+
+    int add(int a, int b){
+        System.out.println("method add 1");
+    }
+    public static void main(String[] args) {
+        
+    }
+}
+// In this code there is an error for method add () because of both methods have same method signatures
