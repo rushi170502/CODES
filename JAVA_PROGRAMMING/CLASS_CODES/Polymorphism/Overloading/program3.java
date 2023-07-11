0ef7d1d75206dd1beb47c95f8aa77912b40a5205
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-07-11 11:04:07 +0530

    In overloading concept return type doesn't matter

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Polymorphism/Overloading/program3.java
@@ -0,0 +1,21 @@
+
+
+class Solution{
+
+    int fun(int x){
+
+
+    }
+
+    void fun(int y){
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+}
+
+// Error: In the above above code the both method name is same and parameter also same so it gives error for that
+// In overloading concept return type doesn't matter so it gives error 
