a84e961449e954b699f380da34d453081d294c7a
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-07-12 10:24:28 +0530

    return type matters in overriding

---
+++ JAVA_PROGRAMMING/CLASS_CODES/Polymorphism/Overriding/program1.java
@@ -0,0 +1,23 @@
+
+
+class Parent{
+
+    int fun(){
+
+    }
+
+}
+class Child extends Parent{
+    void fun(){
+
+    }
+}
+
+class client{
+    public static void main(String[] args) {
+        
+    }
+}
+
+// Error : In overriding concept return type matters 
+// if parent and child have same method then return type required same or co-variant .
