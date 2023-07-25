b8758a37f2262ce31ceb82089170c68e687bc078
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-07-25 11:44:06 +0530

    Thread class sleep() method throws InterruptedException

---
+++ JAVA_PROGRAMMING/CLASS_CODES/17_Exception_Handling/Checked_Exceptions/interruptedExce.java
@@ -0,0 +1,13 @@
+
+
+class Demo{
+    public static void main(String[] args){
+        for(int i = 0; i < 10 ; i++) {
+            System.out.println("i = " + i);
+
+            Thread.sleep(1000);
+        }
+    }
+}
+
+// Thread class sleep() methods throws InterruptedException
