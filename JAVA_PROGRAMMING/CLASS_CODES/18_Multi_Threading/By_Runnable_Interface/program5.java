bd1c71f7e294f6252c597dd79bdbfebac3e1e3e2
rushibirari1705 <hiteshbirari2002@gmail.com>
2023-07-26 20:07:42 +0530

    Creating a Thread using Runnable interface and get details about thread

---
+++ JAVA_PROGRAMMING/CLASS_CODES/18_Multi_Threading/By_Runnable_Interface/program5.java
@@ -0,0 +1,35 @@
+
+
+
+// Creating a Thread using Runnable interface and get details about thread
+
+class MyThread implements Runnable {
+
+    public void run(){ // compulsory need to override this method
+
+        System.out.println("In run");
+        System.out.println(Thread.currentThread());
+    }
+}
+
+class Demo{
+    public static void main(String[] args) {
+        
+        // create a thread
+
+        MyThread obj = new MyThread();
+
+        Thread t = new Thread(obj); // call to Thread(Runnable obj );
+
+        t.start();
+        try{
+            t.join();
+        }catch (InterruptedException ie){
+
+        }
+
+        System.out.println("In Main");
+        System.out.println(Thread.currentThread());
+
+    }
+}
